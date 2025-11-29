26ae0fbdc090ea2852be20e46ed8beb7722abbcf
fancoilaya <fancoilaya@gmail.com>
2025-11-29 19:00:36 +0100

    Update scheduler.py

--- bot/utils/scheduler.py
+++ bot/utils/scheduler.py
@@ -1,12 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from bot.handlers.generator import generate_random_art
-
 
 scheduler = AsyncIOScheduler()
 
-
-
-
 def init_scheduler(app):
-scheduler.add_job(lambda: generate_random_art(app, None), 'cron', hour=9)
-scheduler.start()
+    scheduler.start()
