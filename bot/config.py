181c4695f90757e070d56da8d606de1a2da1354d
fancoilaya <fancoilaya@gmail.com>
2025-11-29 22:25:32 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 import os
 
 STABILITY_API_KEY = os.getenv("STABILITY_API_KEY")
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # unified variable name
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 SCHEDULER_ENABLED = os.getenv("SCHEDULER_ENABLED", "false").lower() == "true"
