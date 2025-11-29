804cd98f302f577f9d3bfd6ba7596a7b5eadcc21
fancoilaya <fancoilaya@gmail.com>
2025-11-29 18:05:52 +0100

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,6 @@
+import os
+
+
+STABILITY_API_KEY = os.getenv("STABILITY_API_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+SCHEDULER_ENABLED = os.getenv("SCHEDULER_ENABLED", "false").lower() == "true"
