eddff6f4dc41105ab3a916e1e8189d2e96a8c9bc
fancoilaya <fancoilaya@gmail.com>
2025-11-29 18:56:50 +0100

    Update commands.py

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,10 +1,5 @@
 from telegram.ext import CommandHandler
 from bot.handlers.generator import handle_grokart
 
-
-
-
-def register_commands(app):commands.py
-
-
-app.add_handler(CommandHandler("grokart", handle_grokart))
+def register_commands(app):
+    app.add_handler(CommandHandler("grokart", handle_grokart))
