c9451b203128669003f7f4107f827718227eb8b3
fancoilaya <fancoilaya@gmail.com>
2025-12-02 09:33:24 +0100

    Update style_free.py

--- bot/utils/style_free.py
+++ bot/utils/style_free.py
@@ -1,5 +1,5 @@
-MEGAGROK_STYLE_FREE = (
-    "retro comic poster of a muscular green frog superhero with glowing orange eyes, "
-    "bold black outlines, heavy shading, fiery orange background, dramatic action pose. "
-    "no realism, no robots, no 3d."
-)
+FREE_STYLE = """
+retro comic poster of a muscular green frog superhero with glowing orange eyes,
+bold black outlines, heavy shading, fiery orange background, dramatic action pose.
+no realism, no robots, no 3d.
+"""
